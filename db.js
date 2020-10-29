e3881b0dd9d6a4844c3add187a39aa9d292e0aa4
vagar123 <vanessa.garnica.14@gmail.com>
2020-10-29 17:34:21 -0500

    Se agrego variable de la conexión a mongo

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+const config = require('./config')
 
 const conectDB = () => {
     /* useNewUrlParser: analiza la informacion que se le quiere enviar a mongo. 
 
     useUnifiedTopology:Escuchar los llmados que hacemos con mongoDB y monitorea que es lo que pasa. 
     */
-    mongoose.connect('',{ useNewUrlParser:true, useUnifiedTopology:true}, (error) =>{
+    mongoose.connect(config.mongoDB,{ useNewUrlParser:true, useUnifiedTopology:true}, (error) =>{
         if(error){
             console.log('Error: ', error)
         }else {
